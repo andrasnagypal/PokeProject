8309761cc52e928142ae9d43b28431c635c3b64c
MacimusRex <nagandsoft@gmail.com>
2020-02-09 21:11:28 +0100

    UI&JSON begin

---
+++ PokeProject/Assets/PokeApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.Networking;
+
+
+public class PokeApiController : MonoBehaviour
+{
+    
+    readonly string basePokeURL = "https://pokeapi.co/api/v2/";
+    int StartIndex = 1, EndIndex = 808;
+    void Start()
+    {
+        for (int i = StartIndex; i < EndIndex; i++)
+        {
+
+        }
+    }
+
+    IEnumerator GetPokemAtIndex(int pokemonIndex)
+    {
+        string pokemonURL = basePokeURL + "pokemon/" + pokemonIndex.ToString();
+
+        UnityWebRequest pokeInfoRequest = UnityWebRequest.Get(pokemonURL);
+    }
+   
+}
