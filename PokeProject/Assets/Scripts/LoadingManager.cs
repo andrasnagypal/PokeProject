0a9c33c2eb9841aa7e2c91e2198708a12caf3331
MacimusRex <nagandsoft@gmail.com>
2020-02-16 19:06:03 +0100

    20200216

---
+++ PokeProject/Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingManager : MonoBehaviour
+{
+    [SerializeField] GameObject Grass1, Grass2, Grass3, Land, RiverGrass, River, Trees;
+    [SerializeField] Vector3 Grass1ScaleTo, Grass2ScaleTo, Grass3ScaleTo, LandScaleTo, RiverGrassScaleTo, RiverScaleTo, TreeScaleTo,PlayerPokeToScale,EnemyPokeToScale,PokePanelToScale;
+    [SerializeField] float MoveUpTime, ScaleUpTime;
+    [SerializeField] GameObject PlayerPoke, EnemyPoke, PokePanel;
+    private void Start()
+    {
+        OnLoadedLevel();
+    }
+    public void OnLoadedLevel()
+    {
+        LeanTween.moveLocalY(Grass1, 0, MoveUpTime);
+        LeanTween.moveLocalY(Grass2, 0, MoveUpTime);
+        LeanTween.moveLocalY(Grass3, 0, MoveUpTime);
+        LeanTween.scale(Grass1, Grass1ScaleTo, ScaleUpTime);
+        LeanTween.scale(Grass2, Grass2ScaleTo, ScaleUpTime);
+        LeanTween.scale(Grass3, Grass3ScaleTo, ScaleUpTime);
+        LeanTween.scale(Land, LandScaleTo, ScaleUpTime).setOnComplete(LoadUpPokeUI);
+    }
+
+
+    public void LoadUpPokeUI()
+    {
+        StageOfTheGame.CurrentStateOfTheGame++;
+    }
+}
---
+++ PokeProject/Assets/Scripts/StageOfTheGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateOfTheGame
+{
+    Loading,
+    ChoseFirstPoke,
+    FirstCombat,
+    EnemyEscaped,
+    EnemyCaptured,
+    PlayerPokeExhausted,
+    Victory,
+    Defeat
+}
+
+public static class StageOfTheGame 
+{
+    public static StateOfTheGame CurrentStateOfTheGame = StateOfTheGame.Loading;
+}
