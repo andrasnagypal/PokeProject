cc2cc9d3e88126a90235261bb0441257eabe655d
MacimusRex <nagandsoft@gmail.com>
2020-03-04 18:22:33 +0100

    káosz

--- PokeProject/Assets/Scripts/PokeModel.cs
+++ PokeProject/Assets/Scripts/PokeModel.cs
@@ -2,6 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DamageRelations :byte
+{
+    no_damage_to,
+    half_damage_to,
+    double_damage_to,
+    no_damage_from,
+    half_damage_from,
+    double_damage_from
+}
 public class PokeModel 
 {
     public string PokeName;
@@ -9,5 +18,6 @@ public class PokeModel
     public string[] TypesOfPoke;
     public string[] AbilitiesOfPoke;
     public int Lvl, Stamina, MaxNumberOfAttacks, CurrentAttackAmount,IndexOfPoke;
+    public byte[] DamageRelationsArray;
 
 }
