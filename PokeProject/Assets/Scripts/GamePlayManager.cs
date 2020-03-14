827d556114e035852bb2d98246b5227666558b3a
MacimusRex <nagandsoft@gmail.com>
2020-03-14 20:14:21 +0100

    20200314

--- PokeProject/Assets/Scripts/GamePlayManager.cs
+++ PokeProject/Assets/Scripts/GamePlayManager.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum GameTurnState :byte
+{
+    Attack,
+    EnemyTurn,
+    Captured,
+    SelectOtherPoke,
+    Lose,
+    Win
+
+}
 public class GamePlayManager : MonoBehaviour
 {
     [SerializeField] PlayerPokeController PPokeController;
     [SerializeField] EnemyPokeController EPokeController;
+    [SerializeField] InfoBarController InfoBar;
+    [SerializeField] GameObject PokeInventoryPanel;
+    GameTurnState StateOfTheGame;
 
+    string[] InfoBarTags = new string[]
+    {
+        "Select an ability to Attack!",
+        "Enemy Poke's turn",
+        "Poke Captured",
+        "Select an other Poke to fight with",
+        "You Lost!",
+        "You Won!" };
     public void GetAnotherPokeForPlayer()
     {
         PPokeController.GetAnotherPoke();
@@ -16,6 +38,14 @@ public class GamePlayManager : MonoBehaviour
     {
         EPokeController.gameObject.SetActive(true);
         EPokeController.GetOtherEnemyPoke();
-       
+        PokeInventoryPanel.SetActive(true);
+        InfoBar.gameObject.SetActive(true);
+        InfoBar.ChangeInfoBar(InfoBarTags[(int)GameTurnState.Attack]);
+        ChangeState(GameTurnState.Attack);
+    }
+
+    public void ChangeState(GameTurnState state)
+    {
+        StateOfTheGame = state;
     }
 }
---
+++ PokeProject/Assets/Scripts/InfoBarController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InfoBarController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI InfoBar;
+
+    public void ChangeInfoBar(string info)
+    {
+        InfoBar.text = info;
+    }
+}
